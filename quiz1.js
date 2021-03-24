b6ba7d967e476af49061f334b2eae6a508be16ea
kobebryant60308 <80386886+kobebryant60308@users.noreply.github.com>
2021-03-24 16:32:43 +0800

    Add files via upload

---
+++ quiz1.js
@@ -0,0 +1,6 @@
+function change()
+{var header = document.getElementById("hid123");     
+header.innerHTML = "CSIE@CGU";    
+var para = document.getElementById("pid123");   
+para.innerHTML = "«ç»ò¨º»ò´Î¡I¡I.";}  
+  function myFunction() {   var btn = document.createElement("BUTTON");   btn.innerHTML = "CLICK ME"; document.body.appendChild(btn).addEventListener("click", change); }
